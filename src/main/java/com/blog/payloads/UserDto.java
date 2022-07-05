33b43dc7901f95324e53e899bebf276401d25da7
Shivansh Vashistha <60701676+vash103@users.noreply.github.com>
2022-07-05 15:38:47 +0530

    Add files via upload

---
+++ src/main/java/com/blog/payloads/UserDto.java
@@ -0,0 +1,46 @@
+package com.blog.payloads;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.blog.entities.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class UserDto {
+
+	private int id;
+
+	@NotEmpty
+	@Size(min = 4, message = "Username must be min of 4 characters !!")
+	private String name;
+
+	@Email(message = "Email address is not valid !!")
+	private String email;
+
+	@NotEmpty
+	@Size(min = 3, max = 10, message = "Passoword must be min of 3 chars and max of 10 chars !!")
+
+
+	private String password;
+
+	@NotEmpty
+	private String about;
+	
+	private Set<RoleDto> roles = new HashSet<>();
+	
+	
+
+}
